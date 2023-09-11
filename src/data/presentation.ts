413fb42ff212daabd0aaf204c0304a4be3bd8922
palden518 <paldenongmo.cst@rub.edu.bt>
2023-09-11 22:01:35 +0600

    feat: vercel analytics

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -16,7 +16,7 @@ const presentation: Presentation = {
   title: "Hi, We’re D101V 👋",
   // profile: "/profile.webp",
   description:
-    "Kuzuzangpola, we're a *Bhutanese Software Development Arm from Royal University of Bhutan* with over *10 years* of joint web experience. We are currently working with *Flutter, NextJS, Remix & Ethereum*.",
+    "Kuzuzangpola, we're a *Bhutanese Software Development Arm from Royal University of Bhutan* 🇧🇹 with over *10 years* of joint web experience. We are currently working with *Flutter, NextJS, Remix & Ethereum*.",
   socials: [
     // {
     //   label: "X",
